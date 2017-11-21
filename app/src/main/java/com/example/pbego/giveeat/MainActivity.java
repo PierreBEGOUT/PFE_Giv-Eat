308bba8c511cc27239736ee887428d788bd080be
Pierre Bégout <pbegout@ensc.fr>
2017-11-21 17:11:12 +0100

    Premières pages

---
+++ app/src/main/java/com/example/pbego/giveeat/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.pbego.giveeat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnTouchListener;
+import android.view.View.OnClickListener;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private Button b = null;
+    private Button a = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.connexion);
+        b.setOnClickListener(this);
+        a=findViewById(R.id.creationCompte);
+        a.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            //si on appuit sur le 1er
+            case R.id.connexion:
+                //action pour le premier bouton
+                break;
+            //si on appuit sur le 2e
+            case R.id.creationCompte:
+                Intent enregistrement = new Intent(MainActivity.this, Enregistrement.class);
+                startActivity(enregistrement);
+            break;
+
+        }
+    }
+
+
+}
+
