40f8bcd2171c89855114cd6cc18a53b26c7fd194
Pierre Bégout <pbegout@ensc.fr>
2017-11-29 17:11:51 +0100

    Fin enregistrement

--- app/src/main/java/com/example/pbego/giveeat/Enregistrement.java
+++ app/src/main/java/com/example/pbego/giveeat/Enregistrement.java
@@ -1,13 +1,27 @@
 package com.example.pbego.giveeat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Enregistrement extends AppCompatActivity {
+public class Enregistrement extends AppCompatActivity implements View.OnClickListener {
+
+    private Button b = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enregistrement);
+        b = findViewById(R.id.suivant);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
+
