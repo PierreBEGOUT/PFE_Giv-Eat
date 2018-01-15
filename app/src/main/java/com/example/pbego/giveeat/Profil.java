c4702176c09dc4a05259b37b724b70ba243ea873
Floranie <floranie@github.com>
2018-01-15 16:30:38 +0100

    Profil

---
+++ app/src/main/java/com/example/pbego/giveeat/Profil.java
@@ -0,0 +1,25 @@
+package com.example.pbego.giveeat;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Profil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+
+        final ImageView ProfilColor = (ImageView) findViewById(R.id.profil_color);
+        ProfilColor.setVisibility(View.VISIBLE);
+
+
+    }
+
+}
