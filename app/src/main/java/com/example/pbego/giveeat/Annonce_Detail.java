157ea7dec7a5fd1245fa624c26441120a9cb77c7
Floranie <floranie@github.com>
2018-01-18 15:22:55 +0100

    détails annonces, mes annonces

---
+++ app/src/main/java/com/example/pbego/giveeat/Annonce_Detail.java
@@ -0,0 +1,13 @@
+package com.example.pbego.giveeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Annonce_Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_annonce__detail);
+    }
+}
